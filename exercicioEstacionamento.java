eea177a9278348faa51ba589f8c26e0b96bd7d59
tiagoalutal <103974210+tiagoalutal@users.noreply.github.com>
2022-05-12 12:08:43 -0300

    Add files via upload

---
+++ exercicioEstacionamento.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+//importando a biblioteca scanner.
+
+ class Estaciomento
+ //nomeando o codigo.
+ {
+    public static void main(String[] args) 
+    {
+        Scanner ler = new Scanner(System.in);
+        //criando o scanner.
+        
+        System.out.print("Digite o numero 1 se voce for gestante,deficiente, idosa// ou 2 para nenhuma das opcoes: "); 
+        //mensagem que ira aparecer para o usuario.
+        
+        int Num;
+        Num = ler.nextInt();
+        //criando uma variavel.
+        
+        if (Num==1)
+        //criando uma condicao.
+        { 
+            System.out.printf("voce pode estacionar na vaga especial");
+            //se a resposta for verdadeira a tela exibe essa mensagem.
+        }
+        
+        else      
+            System.out.printf("voce nao pode estacionar na vaga especial");
+            //se a resposta for falsa a tela exibe essa mensagem.
+     }   
+}
+
